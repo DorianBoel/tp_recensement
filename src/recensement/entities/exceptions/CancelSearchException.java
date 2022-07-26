43bf5bb838bc2224527fae4c8cf34a1cfd0c8344
DorianBoel <dorian.boel@free.fr>
2022-07-26 09:31:11 +0200

    add exceptions javadoc

--- src/recensement/entities/exceptions/CancelSearchException.java
+++ src/recensement/entities/exceptions/CancelSearchException.java
@@ -1,7 +1,15 @@
 package recensement.entities.exceptions;
 
+/**
+ * Exception indicating that a method prompting
+ * the user for input has been cancelled by the
+ * user and must be exited.
+ * 
+ * @author DorianBoel
+ */
 public class CancelSearchException extends Exception {
 
+	@java.io.Serial
 	private static final long serialVersionUID = -2973608392861097872L;
 
 }
--- src/recensement/entities/exceptions/NoResultsException.java
+++ src/recensement/entities/exceptions/NoResultsException.java
@@ -1,7 +1,14 @@
 package recensement.entities.exceptions;
 
+/**
+ * Exception signaling that a search method
+ * returned no results matching a given input.
+ * 
+ * @author DorianBoel
+ */
 public class NoResultsException extends Exception {
 
+	@java.io.Serial
 	private static final long serialVersionUID = 4362619449816310574L;
 
 }
