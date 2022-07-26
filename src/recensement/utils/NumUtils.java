817863bebf8b6dc7e9272bb32ceb63a02c15424b
DorianBoel <dorian.boel@free.fr>
2022-07-27 00:41:02 +0200

    add utils javadoc

--- src/recensement/utils/NumUtils.java
+++ src/recensement/utils/NumUtils.java
@@ -3,8 +3,26 @@ package recensement.utils;
 import java.text.NumberFormat;
 import java.util.Locale;
 
-public class NumUtils {
+/**
+ * Non-instantiable class containing
+ * utility methods used to process
+ * numeric values.
+ * 
+ * @author DorianBoel
+ */
+public final class NumUtils {
 
+	/**
+	 * Don't let anyone instantiate this class.
+	 */
+	private NumUtils() {}
+	
+	/**
+	 * Formats a given integer into a readable String.
+	 * 
+	 * @param i The integer to format
+	 * @return The formatted number string 
+	 */
 	public static String format(int i) {
 		return NumberFormat.getInstance(Locale.FRANCE).format(i);
 	}
--- src/recensement/utils/StringUtils.java
+++ src/recensement/utils/StringUtils.java
@@ -4,20 +4,74 @@ import java.text.Normalizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringUtils {
+/**
+ * Non-instantiable class containing
+ * utility methods used to process
+ * character string values.
+ * 
+ * @author DorianBoel
+ */
+public final class StringUtils {
 	
+	
+	/**
+	 * Don't let anyone instantiate this class.
+	 */
+	private StringUtils() {}
+	
+	/**
+	 * Trims whitespace and escapes special characters
+	 * in a given String.
+	 * 
+	 * @param term The string to sanitized
+	 * @return The sanitized string
+	 */
 	public static String sanitize(String term) {
 		String sanitized = term.strip();
 		sanitized = Pattern.quote(sanitized);
 		return sanitized;
 	}
 
+	/**
+	 * Replaces all letters with diacritical marks
+	 * in a given String with their unmarked versions.
+	 * 
+	 * @param str The string to remove accents from
+	 * @return The string with all accents removed
+	 */
 	public static String stripAccents(String str) {
 	    str = Normalizer.normalize(str, Normalizer.Form.NFD);
 	    str = str.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
 	    return str;
 	}
 	
+	/**
+	 * Checks if a given search term
+	 * matches a given String using a 
+	 * {@link Pattern regex pattern}.
+	 * The pattern is case-insensitive
+	 * and ignores diacritics.
+	 * <p>
+	 * 		The expected values for the flag argument are the following:
+	 * </p>
+	 * <ul>
+	 * 		<li>
+	 * 			<strong>exact</strong>: The match will succeed only if the given String
+	 * 			is identical to the search term.
+	 * 		</li>
+	 * 		<li>
+	 * 			<strong>start</strong>: The match will succeed only if the given String
+	 * 			contains the search term, starting at the first character.
+	 * 		</li>
+	 * </ul>
+	 * 
+	 * @param term The search term used to construct a regex pattern
+	 * @param name The String to attempt to match with the pattern
+	 * @param flag A flag determining how strictly the pattern
+	 * will attempt to match the given String
+	 * @return A {@code boolean} value indicating whether or not the match
+	 * was successful
+	 */
 	public static boolean matchName(String term, String name, String flag) {
 		Pattern pattern = null;
 		String termNoAccents = StringUtils.stripAccents(term);
